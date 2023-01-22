e423bcf11ce9e85155fc00afac2c42e098c80ab4
yakov <82301654+Yakov-kaktus@users.noreply.github.com>
2023-01-22 10:44:35 +0700

    Добавил код бота

--- moderator.py
+++ moderator.py
@@ -1,3 +1,22 @@
-from aiogram import *
 import logging
-from aiogram.types import
+from aiogram import *
+
+# настройки бота
+TOKEN = 'ваш токен'
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+logging.basicConfig(level=logging.INFO)
+black_word = ['плохое слово']
+
+# обработка сообщений
+@dp.message_handler()
+async def send_banned(message: types.Message):
+    chat_id = message.chat.id
+    text = message.text
+    user_id = message.from_user.id
+    lower_text = text.lower()
+    for i in black_word:
+        if i in lower_text:
+            await bot.kick_chat_member(chat_id, user_id)
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
