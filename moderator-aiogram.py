248962f1adeb1d3b9f592f1bd82905afada6d83b
Yakov Voronin <82301654+daymaner@users.noreply.github.com>
2023-05-01 22:25:42 +0700

    Create moderator-aiogram.py

---
+++ moderator-aiogram.py
@@ -0,0 +1,22 @@
+import logging
+from aiogram import *
+
+# настройки бота
+TOKEN = '' #ваш токен
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+logging.basicConfig(level=logging.INFO)
+black_word = [''] #список со словами при появлении которых в сообщении человек будет забанен
+
+# обработка сообщений
+@dp.message_handler()
+async def send_banned(message: types.Message):
+    chat_id = message.chat.id
+    text = message.text
+    user_id = message.from_user.id
+    lower_text = text.lower()
+    for i in black_word:
+        if i in lower_text:
+            await bot.kick_chat_member(chat_id, user_id)
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
